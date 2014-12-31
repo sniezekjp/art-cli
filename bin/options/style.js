77e687da249a90c6c3e6661a804cf606a99e3acc
JP <jsniezek@crowdskout.com>
2014-12-31 11:06:10 -0500

    Added style scss generator

--- bin/options/style.js
+++ bin/options/style.js
@@ -3,11 +3,12 @@ var path = require('path');
 var src  = path.join(process.cwd(), 'src');
 
 module.exports = function(args) {
-  var file = args[3] + '.scss' || 'style.scss';
+  var fileName = args[3] || 'style';
+  var file = fileName + '.scss';
   var styleFile = path.join(src,'modules', args[2], 'scss', file);
   fs.ensureFileSync(styleFile);
 
-  var mainStyle = path.join(src, 'assets', 'css', 'style.scss');
+  var mainStyle = path.join(src, 'assets', 'scss', 'style.scss');
   var content = fs.readFileSync(mainStyle, {encoding: 'utf-8'});
   var replaceHook = '//styles';
   var includeFile = "@import '../../modules/" + args[2] + '/scss/' + file + "';\n" + replaceHook;
