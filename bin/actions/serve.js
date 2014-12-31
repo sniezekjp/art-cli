a5f2d08891041bf03503009263cf92c08f55ed28
JP <jsniezek@crowdskout.com>
2014-12-31 11:29:06 -0500

    Fixed serve command

--- bin/actions/serve.js
+++ bin/actions/serve.js
@@ -2,14 +2,18 @@
 var express = require('express');
 var serve = require('serve-static');
 var path = require('path');
-
 module.exports = function(args) {
   var app = express();
-  app.use(serve(path.join(process.cwd(), 'src')));
+  var serving = path.join(process.cwd(), 'src');
+  app.use(serve(path.join(serving)));
+  app.all('*', function(req, res, next) {
+    console.log('\033[0m404 \033[36m%s\033[0m Assuming SAP, Loading \033[36m%s\033[0m', req.path, 'index.html');
+    next();
+  });
   app.get("*", function(req, res) {
     res.sendFile('index.html', { root: 'src' });
   });
   var port = args[1] || 3000;
   app.listen(port);
-  console.log(port);
+  console.log('\033[0mserving \033[36m%s\033[0m on port \033[96m%d\033[0m', serving, port);
 };
